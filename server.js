af389c7e73edc73058cf2d07c2e1c3c1e2c7f213
Kim Haskell <kim.haskell03@gmail.com>
2020-08-20 18:26:28 -0600

    updating server.js

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.connect(MONGODB_URI)
 
 // routes
 app.use(require("./routes/api.js"));
